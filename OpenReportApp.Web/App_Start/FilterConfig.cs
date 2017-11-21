7a1975a8ae4cad3778eddb4dc90d308f25afc111
sreeharikv <sreeharikv@gmail.com>
2017-11-21 17:01:25 -0500

    Adding infrastructre files and folders

---
+++ OpenReportApp.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OpenReportApp.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+        }
+    }
+}
---
+++ OpenReportApp.Web/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing; 
+
+namespace OpenReportApp.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles); 
+        }
+    }
+}
